40ab1fb80dc72e3a23583ba62cb01b30cc316e63
Jorge Guerrero <giorg0s@pm.me>
2020-04-03 13:01:13 +0200

    Update deteccion_orb.py

--- deteccion_orb.py
+++ deteccion_orb.py
@@ -2,12 +2,27 @@ import cv2
 import numpy as np
 import os
 import time
+import sys
 from matplotlib import pyplot as plt
 
 CARPETA_TRAIN = 'train'
 CARPETA_TEST = 'test'
 
+# Array para almacenar las imagenes de entrenamiento leidas
 imagenes_train = []
+
+# Lo mismo para las imagenes de test
+imagenes_test = []
+
+# FLANN parameters
+FLANN_INDEX_LSH = 6
+index_params = dict(algorithm=FLANN_INDEX_LSH,
+                    table_number=12,  # 12
+                    key_size=14,  # 20
+                    multi_probe_level=2)  # 2
+
+search_params = dict(checks=100)  # or pass empty dictionary
+
 
 def carga_imagenes_carpeta(nombre_carpeta):
     print("Se va a iniciar la carga de las imagenes de", nombre_carpeta)
@@ -18,24 +33,52 @@ def carga_imagenes_carpeta(nombre_carpeta):
         if imagen is not None:
             imagenes_train.append(imagen)
             print("He leido la imagen ", nombre_imagen)
-            time.sleep(.100)
+            # time.sleep(.500)
     print("###################################################")
     print("FIN")
+    print()
+    time.sleep(1)
+    
     return imagenes_train
 
-def deteccion_orb(imagen):
+
+def deteccion_orb(imagenes):
+    # se crea el detector ORB
     orb = cv2.ORB_create(nfeatures=100, nlevels=4, scaleFactor=1.3)
-    keypoints = orb.detect(imagen, None)
 
-    keypoints, des = orb.compute(imagen, keypoints)
+    keypoints = []
 
-    # draw only keypoints location,not size and orientation
-    img2 = cv2.drawKeypoints(imagen, keypoints, None, color=(255, 0, 255), flags= 0)
-    plt.imshow(img2), plt.show()
+    # Se crea el matcher Flann
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+    print("Se va a iniciar la detección ORB")
+    print("###################################################")
+    time.sleep(2)
+
+    # Se detectan los puntos de interes y se computan los descriptores con ORB
+    for i in range(len(imagenes)):
+        print("ORB para", i)
+        (kp, des) = orb.detectAndCompute(imagenes[i], None)
+        keypoints.append(kp)
+        flann.add([des])
+
+    # A knnMatch hay que pasarle la estructura con los descriptores almacenados
+    matches = flann.knnMatch(np.array([[8, 8, 8]], dtype=np.uint8), k=1)
+    
+    print("###################################################")
+    print("FIN")
+    print()
+    time.sleep(1)
+    
+    for m in matches:
+        for n in m:
+            print("Res - dist:", n.distance, " img: ", n.imgIdx, " queryIdx: ", n.queryIdx, " trainIdx:", n.trainIdx)
 
 
-carga_imagenes_carpeta(CARPETA_TRAIN)
-deteccion_orb(imagenes_train[15])
-print(len(imagenes_train))
+def main():
+    imgs = carga_imagenes_carpeta(CARPETA_TRAIN)
+    deteccion_orb(imgs)
 
 
+if __name__ == '__main__':
+    main()
