d72af7ed6fae7ab80cc576e169ef3a00e2cbfce8
Jorge Guerrero <giorg0s@pm.me>
2020-04-06 22:01:40 +0200

    El entrenamiento debería estar acabado

--- deteccion_orb.py
+++ deteccion_orb.py
@@ -8,13 +8,7 @@ from matplotlib import pyplot as plt
 CARPETA_TRAIN = 'train'
 CARPETA_TEST = 'test'
 
-# Array para almacenar las imagenes de entrenamiento leidas
-imagenes_train = []
-
-# Lo mismo para las imagenes de test
-imagenes_test = []
-
-# FLANN parameters
+# Parámetros FLANN
 FLANN_INDEX_LSH = 6
 index_params = dict(algorithm=FLANN_INDEX_LSH,
                     table_number=12,  # 12
@@ -23,8 +17,16 @@ index_params = dict(algorithm=FLANN_INDEX_LSH,
 
 search_params = dict(checks=100)  # or pass empty dictionary
 
+# Almacenamiento de puntos de interes
+training_keypoints = []
+
+# Estructura para almacenar los descriptores (basada en Flann)
+flann = cv2.FlannBasedMatcher(index_params, search_params)
+
 
 def carga_imagenes_carpeta(nombre_carpeta):
+    imagenes = []
+
     print("Se va a iniciar la carga de las imagenes de", nombre_carpeta)
     print("###################################################")
     time.sleep(2)
@@ -32,7 +34,7 @@ def carga_imagenes_carpeta(nombre_carpeta):
     for nombre_imagen in os.listdir(nombre_carpeta):
         imagen = cv2.imread(os.path.join(nombre_carpeta, nombre_imagen), 0)
         if imagen is not None:
-            imagenes_train.append(imagen)
+            imagenes.append(imagen)
             print("He leido la imagen ", nombre_imagen)
             # time.sleep(.500)
     print("###################################################")
@@ -40,45 +42,60 @@ def carga_imagenes_carpeta(nombre_carpeta):
     print()
     time.sleep(1)
 
-    return imagenes_train
+    return imagenes
 
 
-def deteccion_orb(imagenes):
+def entrenamiento_orb(training_imgs):
     # se crea el detector ORB
     orb = cv2.ORB_create(nfeatures=100, nlevels=4, scaleFactor=1.3)
 
-    keypoints = []
-
-    # Se crea el matcher Flann
-    flann = cv2.FlannBasedMatcher(index_params, search_params)
-
     print("Se va a iniciar la detección ORB")
     print("###################################################")
-    time.sleep(2)
+    #time.sleep(2)
 
     # Se detectan los puntos de interes y se computan los descriptores con ORB
-    for i in range(len(imagenes)):
+    for i, img in enumerate(training_imgs):
         print("ORB para", i)
-        (kp, des) = orb.detectAndCompute(imagenes[i], None)
-        keypoints.append(kp)  # se guarda la informacion de cada keypoint
+        (kp, des) = orb.detectAndCompute(img, None)
+        training_keypoints.append(kp)  # se guarda la informacion de cada keypoint
         flann.add([des])  # se almacenan los descriptores
-
-    # A knnMatch hay que pasarle la estructura con los descriptores almacenados
-    matches = flann.knnMatch(np.array([[8, 8, 8]], dtype=np.uint8), k=1)
 
     print("###################################################")
     print("FIN")
     print()
-    time.sleep(1)
+    #time.sleep(1)
 
-    for m in matches:
-        for n in m:
-            print("Res - dist:", n.distance, " img: ", n.imgIdx, " queryIdx: ", n.queryIdx, " trainIdx:", n.trainIdx)
-            
+
+def procesamiento_img(test_imgs):
+    orb = cv2.ORB_create(nfeatures=100, nlevels=4, scaleFactor=1.3)
+    vector_votacion = []
+
+    # Ha knnMatch hay que pasarle la estructura con los descriptores almacenados
+    for i, img in enumerate(test_imgs):
+        print("Para la imagen de test", i)
+
+        (kp, des) = orb.detectAndCompute(img, None)
+
+        matches = flann.knnMatch(des, k=2)
+
+
+        # HAY QUE DEFINIR LA VOTACION DE HOUGH PARA QUEDARSE CON EL MEJOR DESCRIPTOR
+
+        #img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=2)
+        #plt.imshow(img3)
+
+        # Se compara el descriptor de la imagen leida con todos los que contiene la estructura Flann (los coches que ya conoce)
+        #for m in matches:
+            #for n in m:
+                #print("Res - dist:", n.distance, " img: ", n.imgIdx, " queryIdx: ", n.queryIdx, " trainIdx:", n.trainIdx)
+
 
 def main():
-    imgs = carga_imagenes_carpeta(CARPETA_TRAIN)
-    deteccion_orb(imgs)
+    training_imgs = carga_imagenes_carpeta(CARPETA_TRAIN)
+    test_imgs = carga_imagenes_carpeta(CARPETA_TEST)
+
+    entrenamiento_orb(training_imgs)
+    procesamiento_img(test_imgs)
 
 
 if __name__ == '__main__':
