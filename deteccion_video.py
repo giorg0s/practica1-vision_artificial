351db7e2ffadf23392775df59d2e5eea427db7b6
Jorge Guerrero <giorg0s@pm.me>
2020-04-08 16:16:05 +0200

    Se ha añadido el cascade classifier para vídeo

--- deteccion_video.py
+++ deteccion_video.py
@@ -4,13 +4,18 @@ import cv2
 import time
 import os
 import numpy as np
+from deteccion_haar import *
 
 CARPETA_VIDEOS = "videos/"
 
+out = cv2.VideoWriter("output.avi", cv2.VideoWriter_fourcc(*'mp4v'), 30.0, (640, 480))
+
 
 def carga_videos_carpeta(nombre_video):
-    video_cap =cv2.VideoCapture(CARPETA_VIDEOS+nombre_video)
+    video_cap = cv2.VideoCapture(CARPETA_VIDEOS+nombre_video)
     contador_frames = 1
+
+    cascade = cv2.CascadeClassifier(CLASIFICADOR)
 
     print("Se va a iniciar la carga del vídeo", nombre_video)
     print("###################################################")
@@ -22,13 +27,20 @@ def carga_videos_carpeta(nombre_video):
 
         if ret:
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            cv2.imshow(nombre_video, gray)
-            key = cv2.waitKey(1)
+            coche = cascade.detectMultiScale(gray, 1.1, 1)
+            if coche is ():
+                print("No se ha encontrado coche")
+            for (x, y, w, h) in coche:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.imshow('Detector de coches', frame)
+                out.write(cv2.resize(frame, dsize=(640,480), interpolation=cv2.INTER_CUBIC))
+                cv2.waitKey(1)
         else:
             break
 
     print("FIN")
     video_cap.release()
+    out.release()
     cv2.destroyAllWindows()
 
     print("###################################################")
